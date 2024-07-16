9c2edca2b84740a6f316f930fa85941569442bb5
yuxin4667 <yuxin4667@gmail.com>
2024-07-16 15:28:40 +0800

    氣泡排序

---
+++ bubbleSort.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a[10] = { 5, 7, 2, 3, 8, 6, 1, 9, 4, 10};
+    int arrayLength = sizeof(a) / sizeof(*a);
+    for (int i = arrayLength - 1; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] > a[i]) {
+                int temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+
+    cout << "--- After Bubble Sort ---"<< endl;
+    for (int i = 0; i < arrayLength; i++) {
+        cout << a[i] << " -> ";
+    }
+    return 0;
+}
+
